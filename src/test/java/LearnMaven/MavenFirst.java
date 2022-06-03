1025e2326b3d2e436534802a09b204d491b06a98
vikash <vikashraj033@gmail.com>
2022-06-03 11:23:46 +0530

    My first commit

---
+++ src/test/java/LearnMaven/MavenFirst.java
@@ -0,0 +1,10 @@
+package LearnMaven;
+
+import org.junit.Test;
+
+public class MavenFirst {
+	@Test
+	public void MavenMethod2() {
+		System.out.println("Hello its second maven method");
+	}
+}
---
+++ src/test/java/LearnMaven/MavenSecondTest.java
@@ -0,0 +1,11 @@
+package LearnMaven;
+
+import org.testng.annotations.Test;
+
+public class MavenSecondTest {
+
+	@Test
+	public void MavenMethod1() {
+		System.out.println("Hello its first maven method");
+	}
+}
